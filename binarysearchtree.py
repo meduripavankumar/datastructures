23bf766ec687d9d16b5fecc3c0cc837ef6155c82
meduripavankumar <contactpavankumarm@gmail.com>
2025-02-20 09:48:20 +0530

    Update binarysearchtree.py

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self,val):
+    def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
@@ -8,32 +8,33 @@ class BST:
     def __init__(self):
         self.root = None
     
-    def insert(self,val):
+    def insert(self, val):
         new_node = Node(val)
         if self.root is None:
             self.root = new_node
             return True
         cur = self.root
         while True:
-            if new_node.val < cur.val:
+            if val < cur.val:
                 if cur.left is None:
                     cur.left = new_node
                     return True
                 cur = cur.left
-            elif new_node.val > cur.val:
+            elif val > cur.val:
                 if cur.right is None:
                     cur.right = new_node
                     return True
                 cur = cur.right
             else:
-                return False
-            
+                return False  # Avoid inserting duplicates
+
     def BFS(self):
-        res,queue = []
         if self.root is None:
-            return None
-        cur = self.root
-        queue.append(cur)
+            return []  # Return an empty list instead of None
+        
+        res = []
+        queue = [self.root]  # Initialize queue properly
+        
         while len(queue) > 0:
             cur = queue.pop(0)
             res.append(cur.val)
@@ -43,50 +44,57 @@ class BST:
                 queue.append(cur.right)
         return res
     
-
-# i left the root right
-# po root the left right
-
+    # DFS Inorder (Left -> Root -> Right)
     def dfs_inorder(self):
         res = []
         def traverse(node):
-            if node.left:
-                traverse(node.left)
+            if not node:
+                return
+            traverse(node.left)
             res.append(node.val)
-            if node.right:
-                traverse(node.right)
+            traverse(node.right)
         traverse(self.root)
         return res
     
+    # DFS Postorder (Left -> Right -> Root)
     def dfs_postorder(self):
         res = []
         def traverse(node):
-            if node.left:
-                traverse(node.left)
-            if node.right:
-                traverse(node.right)
+            if not node:
+                return
+            traverse(node.left)
+            traverse(node.right)
             res.append(node.val)
         traverse(self.root)
         return res
 
+    # DFS Preorder (Root -> Left -> Right) [Fixed order]
     def dfs_preorder(self):
         res = []
         def traverse(node):
-            if node.left:
-                traverse(node.left)
-            if node.right:
-                traverse(node.right)
-            res.append(node.val)
+            if not node:
+                return
+            res.append(node.val)  # Root first
+            traverse(node.left)
+            traverse(node.right)
         traverse(self.root)
         return res
 
+# Testing the corrected BST implementation
 my_bst = BST()
 
+# Insert elements into the BST
 my_bst.insert(2)
 my_bst.insert(1)
 my_bst.insert(3)
-print(my_bst.root.val)
-print(my_bst.root.right.val)
-print(my_bst.root.left.val)
-# print(my_bst.root.right.right.val)
 
+# Check root values
+print("Root Value:", my_bst.root.val)           # Expected: 2
+print("Right Child:", my_bst.root.right.val)    # Expected: 3
+print("Left Child:", my_bst.root.left.val)      # Expected: 1
+
+# Perform BFS and DFS Traversals
+print("\nBFS Traversal:", my_bst.BFS())         # Expected: [2, 1, 3]
+print("DFS Inorder:", my_bst.dfs_inorder())     # Expected: [1, 2, 3] (sorted order)
+print("DFS Preorder:", my_bst.dfs_preorder())   # Expected: [2, 1, 3] (Root -> Left -> Right)
+print("DFS Postorder:", my_bst.dfs_postorder()) # Expected: [1, 3, 2] (Left -> Right -> Root)
